6248fdc3addf43cb1dfa18028e2a32bfb12c9a64
trojan-9600 <acharyaharhjit001@gmail.com>
2020-06-29 02:17:34 +0530

    Updates

---
+++ precache-manifest.a1df3de1678e457b778cad015d74173f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccaca7ef0a4cb8801176319114b630bc",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "ee15695d5f61debd75b9",
+    "url": "/todoapp/static/css/main.4a882603.chunk.css"
+  },
+  {
+    "revision": "5369231de70aa89e3314",
+    "url": "/todoapp/static/js/2.47293ffc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.47293ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee15695d5f61debd75b9",
+    "url": "/todoapp/static/js/main.f1453146.chunk.js"
+  },
+  {
+    "revision": "a0dc61240e341eaac320",
+    "url": "/todoapp/static/js/runtime-main.ec8ab811.js"
+  }
+]);
